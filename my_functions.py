44320982f2d79d3fb1c92fac2b68b0385d3d4c4e
Bharat Ram Ammu <ammubharatram@gmail.com>
2019-12-19 03:14:07 +0100

    Update my_functions.py

--- my_functions.py
+++ my_functions.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
+# =============================================================================
+# Module Level variables
+# =============================================================================
+
+
+sticked = None
+filled=None
+filled_stick=None
+
+
+
+
 """
 Input
 x: existing x-coordinate of particle
@@ -9,7 +21,9 @@ m : dimensions of the matrix
 import numpy as np
 import random
 from random import choices
+
 def change_coordinates(x,y,m):
+    
     # =============================================================================
     # Moving the collided particle randomly to neighboruing cells like earlier
     # =============================================================================
@@ -18,14 +32,21 @@ def change_coordinates(x,y,m):
         y_motion = y+ np.random.choice([-1,1]) 
     else: # if x-position changed, y-position can but need not change - hence -1,0,1 (including 0)
         y_motion = y+ np.random.choice([-1,0,1])   
-    
+    print("coordinates are now changed")
     #for particle being in border, account for toroidal bounding or bouncing: particle either bounces little or much
     # (0,m-1): 0 and m-1 refer to toroidal bounding respecitively for both conditions
     # other values refer to bouncing randomly
-    if x_motion<0 or x_motion>m-1:
-        x_motion = random.randint(0,m-1)
-    if y_motion<0 or y_motion>m-1:
-        y_motion = random.randint(0,m-1)
+    
+    # account for toroidal bounding
+    #if coordinate exceeds the last row/column, it goest to first row/column respectively
+    if x_motion<0:
+        x_motion = m-1
+    if y_motion<0:
+        y_motion = m-1
+    if x_motion>m-1:
+        x_motion = 0 #if coordinate exceeds the last row/column, it goest to first row/column respectively
+    if y_motion>m-1:
+        y_motion= 0
     #print(matrix)
     #print(matrix[x_motion,y_motion])
     # Updated coordinates of particle
@@ -71,71 +92,143 @@ def move_until_it_sticks(matrix,x_new,y_new,x_start,y_start,m,k,iterable,no_stic
     # =============================================================================
     #  while this new position is empty /not occupied or particle did not stick: move to new coordinates
     # =============================================================================
-    if matrix[x_new,y_new]!=1: #while this new position is empty /not occupied
-        matrix[x_start,y_start]=0 #remove particle from old or starting position
-        matrix[x_new,y_new]=1 #place particle in new motion position
-        x_start = x_new #change motion coordinates to old coordinates, for while loop to restart
-        y_start= x_new #change motion coordinates to old coordinates, for while loop to restart
-        # motion particle becomes old particle for next iteration!
-            
-    elif matrix[x_new,y_new]==1:
-        # =============================================================================
-        # Check once it hits, it sticks or not based on sticking probability 'k'
-        # =============================================================================
-        from my_functions import stickiness_choice
-        stick_flag,stick_flag_text = stickiness_choice(k) #written in my_functions
+    
+    
+    
+    stuck_moved= False  #when particle stuck and moved to another position, we don't repeat while loop
+    while(stuck_moved==False):
         
-        # =============================================================================
-        # If it didn't stick, repeat the process of moving to new coordinates
-        # =============================================================================
-        if (stick_flag==[0]):
-            from my_functions import change_coordinates
-            x_new,y_new = change_coordinates(x_new,y_new,m)   #updated coordinates after movement              
-            move_until_it_sticks(matrix,x_new,y_new,x_start,y_start,m,k,iterable,no_stick=True) #recurse with updated coordinates
-        # =============================================================================
-        #         the particle stuck with another particle
-        # =============================================================================
-        else: # meaning the particle stuck with another particle
-            x_finish = x_new+ np.random.choice([-1,0,1])
-            if x_finish == x_new:  # if new x-position doesn't change, ensure new y- position changes for sure- no 0
-                y_finish = y_new+ np.random.choice([-1,1]) 
-            else: # if x-position changed, y-position can but need not change - hence -1,0,1 (including 0)
-               y_finish = y_new+ np.random.choice([-1,0,1]) 
+        if matrix[x_new,y_new]!=1: #while this new position is empty /not occupied
+                matrix
+                matrix[x_start,y_start]=0 #remove particle from old or starting position
+                matrix[x_new,y_new]=1 #place particle in new motion position
+                x_old = x_new #change motion coordinates to old coordinates, for while loop to restart
+                y_old= y_new #change motion coordinates to old coordinates, for while loop to restart
+                # motion particle becomes old particle for next iteration!
+                print("particle occupied empty position")
+                iterable+=1 #this is one iteration
+                stuck_moved= True
+                break
+                    
+        elif matrix[x_new,y_new]==1:
+                stuck_moved = False
+                # =============================================================================
+                # Check once it hits, it sticks or not based on sticking probability 'k'
+                # =============================================================================
+                print("Particle hit another particle, but let's see if it sticks!")
+                from my_functions import stickiness_choice
+                stick_flag,stick_flag_text = stickiness_choice(k) #written in my_functions
+                
+                # =============================================================================
+                # If it didn't stick, repeat the process of moving to new coordinates
+                # =============================================================================
+                if (stick_flag==[0]):
+                    from my_functions import change_coordinates
+                    x_old= x_new
+                    y_old= y_new
+                    x_new,y_new = change_coordinates(x_new,y_new,m)   #updated coordinates after movement              
+                    
+                    stuck_moved = False #recurse with updated coordinates
+                    
+                    
+                # =============================================================================
+                #         the particle stuck with another particle
+                # =============================================================================
+                else: # meaning the particle stuck with another particle
+                    #x_finish = x_start
+                    #y_finish = y_start
+                    #matrix[x_start,y_start]=0 #remove particle from old position, which is starting position
+                    #matrix[x_finish,y_finish]=1 #stick particle to existing particles
+                     #stick particle to existing particles- let it be in old position- x_start
+                    #stick_particle(m,matrix,x_new,y_new,x_start,y_start)
+                    x_old = x_new
+                    y_old= y_new
+                    stuck_moved = True #stop internal while loop, as particle now stuck
+                    no_stick= False #stop the bigger while loop using this global variable, as particle now stikcs
+                    
+    return (matrix,x_old,y_old,no_stick)
+
+def initiate_new_particle(m,matrix):
+    filled=True
+    while (filled==True):
         
+        x_start= random.randint(0,m-1)
+            
+        if x_start in (0,m-1):
+            
+            y_start= random.randint(0,m-1)
+        else:
+            
+            y_start= random.choice([0,m-1])
+        print(x_start,y_start)
+        print(matrix[x_start,y_start])
+        if (matrix[x_start,y_start]==0): #if it is empty, take that place
+            print('empty cell found')
+            
+            filled= False
+            print("filled flag is now {}".format(filled))
+            matrix[x_start,y_start]=1
+            return x_start,y_start,matrix
+            continue
+        else:                           # if occupied, try initiating in another place, again in the border
+            
+            filled= True                   # while loop repeats, hence initiation restarts
+            print("filled flag is now {}".format(filled))
+            print("initiation restarts")
+       
         
-            # =============================================================================
-            #    for particle sticking another which is in border, it can stick in less sides
-            # =============================================================================
-            if x_finish<0 and y_finish!=y_new:
-                x_finish =np.random.choice([0,1])
-            if x_finish<0 and y_finish==y_new:
-                x_finish =1 #as it can't be in same cell as hitting particle
-               
-            if x_finish>m-1 and y_finish!=y_new:
-                x_finish=np.random.choice([m-2,m-1])
-            if x_finish>m-1 and y_finish==y_new:
-                x_finish=m-2 #as it can't be in same cell as hitting particle
-                
+    
+
+def stick_particle(m,matrix,x_new,y_new,x_start,y_start):
+    filled_stick=True
+    while (filled_stick):
+        x_finish = x_new+ np.random.choice([-1,0,1])
+        if x_finish == x_new:  # if new x-position doesn't change, ensure new y- position changes for sure- no 0
+            y_finish = y_new+ np.random.choice([-1,1]) 
+        else: # if x-position changed, y-position can but need not change - hence -1,0,1 (including 0)
+           y_finish = y_new+ np.random.choice([-1,0,1]) 
+    
+    
+        # =============================================================================
+        #    for particle sticking another which is in border, it can stick in less sides
+        # =============================================================================
+        if x_finish<0 and y_finish!=y_new: #<0 happens when x_new =0
+            x_finish =np.random.choice([0,1])
+        if x_finish<0 and y_finish==y_new:
+            x_finish =1 #as it can't be in same cell as hitting particle as x_new=0
+           
+        if x_finish>m-1 and y_finish!=y_new: #<m-1 happens when x_new =m-1
+            x_finish=np.random.choice([m-2,m-1])
+        if x_finish>m-1 and y_finish==y_new:
+            x_finish=m-2 #as it can't be in same cell as hitting particle
             
-            if y_finish<0 and x_finish!=x_new:
-                 y_finish = np.random.choice([0,1])
-            if y_finish<0 and x_finish==x_new:
-                y_finish=1 #as it can't be in same cell as hitting particle
-                
-                
-            if y_finish>m-1 and x_finish!=x_new:
-                y_finish=np.random.choice([m-2,m-1]) 
-            if y_finish>m-1 and x_finish==x_new:
-                y_finish=m-2 #as it can't be in same cell as hitting particle
+        
+        if y_finish<0 and x_finish!=x_new:
+             y_finish = np.random.choice([0,1])
+        if y_finish<0 and x_finish==x_new:
+            y_finish=1 #as it can't be in same cell as hitting particle
             
             
-            # =============================================================================
-            # moving particle from initial to final position
-            # =============================================================================
-            matrix[x_start,y_start]=0 #remove particle from old or starting position
+        if y_finish>m-1 and x_finish!=x_new:
+            y_finish=np.random.choice([m-2,m-1]) 
+        if y_finish>m-1 and x_finish==x_new:
+            y_finish=m-2 #as it can't be in same cell as hitting particle
+        
+        print(x_finish,y_finish)
+        print(matrix[x_finish,y_finish])
+        # =============================================================================
+        # moving particle from initial to final position
+        # =============================================================================
+        if matrix[x_finish,y_finish]==0: #if it is empty, take that place around neighborhood to stuck particle
+            
+            filled_stick= False
+            matrix[x_start,y_start]=0 #remove particle from old position, which is starting position
             matrix[x_finish,y_finish]=1 #stick particle to existing particles
-            no_stick= False #stop while loop, as particle now stikcs
+            print("Sticked particle in neighborhood of collided particle")
+            break
+        
+        else:                       # if particle exists, repeat sticking location in some other position within neighborhood of particle itself
             
-            #print(matrix[x_finish,y_finish])
-            print("The number of iterations before particle stuck are {}".format(iterable))
-    return (matrix,x_start,y_start,no_stick)
+            filled_stick= True # repeat to stick in an empty neighboring cell
+            print("can't stick it here, look for another sticking coordinates")
+            
